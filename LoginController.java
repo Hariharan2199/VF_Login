d54b022805f4b4e58928842c56e43d4bdece2baf
Hariharan2199 <39941448+Hariharan2199@users.noreply.github.com>
2019-07-29 16:07:18 +0530

    Create LoginController.java

---
+++ LoginController.java
@@ -0,0 +1,17 @@
+package com.log.login.LoginController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class LoginController {
+
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+
+    }
+
+
+}
